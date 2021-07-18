c319b7ab04f74bd81c75ae9d974056c306bc8624
monojitwithu <monojitwithu@gmail.com>
2021-07-18 20:58:35 +0530

    hh

--- src/components/common/cart/Cart.js
+++ src/components/common/cart/Cart.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 
 
-const Cart=({visibility,num3,Product,total})=>{
+const Cart=({visibility,Product,onAdd})=>{
+
+    console.log(Product)
     
    
     
@@ -13,40 +15,43 @@ const Cart=({visibility,num3,Product,total})=>{
     return(
         <div className={ `modal ${visibility===true?'visible':""}`}>
             
+            
         <div className="c-box">
+        <h2 style={{marginLeft:"232px"}}>Cart Item</h2>
+        <div className="cart-product"></div>  
+    
+
+
+        
+        <div    >    
+            {Product.length===0 && <div>Cart is Empty</div>}    
+        
+        {Product.map((item)=>(
+            <div key={item.id}    style={{ justifyContent:"space-around",display:"flex",width:"100%",marginBottom:"6px" }}>
+                <div >{item.name}</div>
+              
+                
+                
+                <div style={{display:"flex"}}> <button onClick={()=>onAdd(item)} className="add" style={{background:"#3cd53f",border:"none", marginRight:"3px"}}>+</button>
+                <div><button  className="add">-</button></div>
+                
+                
+                </div>
+                
+
+                <div> <span style={{color:"red"}}>Qty</span> {item.qty} <span style={{color:"red"}}>X</span> ₹{item.price}</div>
+            
+            
+            </div>
+        ))}
+
+
+</div >
         
             
             
             
-            {Product.map(({name,priceOfProduct,image},index)=> {
-                
-                
-                
-                return(<div className="i-box">
-                <img src={image} alt='product-image '/>
-                <p>{name}</p>
-    
-                <a className='i-price' href="#">Rs.{priceOfProduct}</a>
-                </div>)
-                
-                }
-                
-                
-                
-
-
-
-                
-
-
-
-
-
-
-
-            
-            )
-            }
+          
             
             
             
